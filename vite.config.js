c6d342de24361fc40c9ed8480e2932425b7c45df
teaplz <61762655+teaplz@users.noreply.github.com>
2024-10-02 06:36:40 -0400

    Update PWA manifest and graphics

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
@@ -20,31 +20,34 @@ export default defineConfig(({mode}) => {
           name: "Unicode Mahjong Tile Solitaire",
           icons: [
             {
-              src: "favicon.ico",
-              sizes: "48x48 32x32 16x16",
-              type: "image/x-icon",
-            },
-            {
-              src: "icon-192.png",
-              type: "image/png",
+              src: "/pwa-192x192.png",
               sizes: "192x192",
+              type: "image/png",
+              purpose: "any",
             },
             {
-              src: "icon-512.png",
-              type: "image/png",
+              src: "/pwa-512x512.png",
               sizes: "512x512",
+              type: "image/png",
+              purpose: "any",
             },
             {
-              src: "icon-512m.png",
+              src: "/pwa-maskable-192x192.png",
+              sizes: "192x192",
               type: "image/png",
+              purpose: "maskable",
+            },
+            {
+              src: "/pwa-maskable-512x512.png",
               sizes: "512x512",
+              type: "image/png",
               purpose: "maskable",
             },
           ],
           start_url: ".",
           display: "standalone",
-          theme_color: "#6c9382",
-          background_color: "#6c9382",
+          theme_color: "#657085",
+          background_color: "teal",
         },
       }),
     ],
