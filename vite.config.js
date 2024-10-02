8707c41a409ed5e0300b10d68607515b6a59d7df
teaplz <61762655+teaplz@users.noreply.github.com>
2024-10-02 13:21:52 -0400

    Update PWA manifest
    - Fix icons not loading.
    - Remove unused icon.
    - Update background color.

--- vite.config.js
+++ vite.config.js
@@ -13,32 +13,31 @@ export default defineConfig(({ mode }) => {
         includeAssets: [
           "favicon.ico",
           "apple-touch-icon.png",
-          "icon-safari-pinned-tab.svg",
         ],
         manifest: {
           short_name: "UMTSolitaire",
           name: "Unicode Mahjong Tile Solitaire",
           icons: [
             {
-              src: "/pwa-192x192.png",
+              src: "pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
               purpose: "any",
             },
             {
-              src: "/pwa-512x512.png",
+              src: "pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any",
             },
             {
-              src: "/pwa-maskable-192x192.png",
+              src: "pwa-maskable-192x192.png",
               sizes: "192x192",
               type: "image/png",
               purpose: "maskable",
             },
             {
-              src: "/pwa-maskable-512x512.png",
+              src: "pwa-maskable-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "maskable",
@@ -47,7 +46,7 @@ export default defineConfig(({ mode }) => {
           start_url: ".",
           display: "standalone",
           theme_color: "#657085",
-          background_color: "teal",
+          background_color: "#153737",
         },
       }),
     ],
